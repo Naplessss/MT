c3795089ef1991fc065cfca100e02aa4fc0aa65b
Shun Zheng <shun.zhen@microsoft.com>
2021-04-02 10:20:48 +0000

    init

---
+++ src/dataset.py
@@ -0,0 +1,51 @@
+import cv2
+import pandas as pd
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
+import torch
+import os
+
+class TrainDataset(Dataset):
+    def __init__(self, df, tokenizer, transform=None):
+        super().__init__()
+        self.df = df
+        self.tokenizer = tokenizer
+        self.file_paths = df['file_path'].values
+        self.labels = df['InChI_text'].values
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        file_path = self.file_paths[idx]
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented['image']
+        label = self.labels[idx]
+        label = self.tokenizer.text_to_sequence(label)
+        label_length = len(label)
+        label_length = torch.LongTensor([label_length])
+        return image, torch.LongTensor(label), label_length
+
+
+class TestDataset(Dataset):
+    def __init__(self, df, transform=None):
+        super().__init__()
+        self.df = df
+        self.file_paths = df['file_path'].values
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        file_path = self.file_paths[idx]
+        image = cv2.imread(file_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        if self.transform:
+            augmented = self.transform(image=image)
+            image = augmented['image']
+        return image
---
+++ src/utils.py
@@ -0,0 +1,137 @@
+import os
+import random
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
+import Levenshtein
+import torch
+import numpy as np
+import math
+import time
+
+
+class Tokenizer(object):
+
+    def __init__(self):
+        self.stoi = {}
+        self.itos = {}
+
+    def __len__(self):
+        return len(self.stoi)
+
+    def fit_on_texts(self, texts):
+        vocab = set()
+        for text in texts:
+            vocab.update(text.split(' '))
+        vocab = sorted(vocab)
+        vocab.append('<sos>')
+        vocab.append('<eos>')
+        vocab.append('<pad>')
+        for i, s in enumerate(vocab):
+            self.stoi[s] = i
+        self.itos = {item[1]: item[0] for item in self.stoi.items()}
+
+    def text_to_sequence(self, text):
+        sequence = []
+        sequence.append(self.stoi['<sos>'])
+        for s in text.split(' '):
+            sequence.append(self.stoi[s])
+        sequence.append(self.stoi['<eos>'])
+        return sequence
+
+    def texts_to_sequences(self, texts):
+        sequences = []
+        for text in texts:
+            sequence = self.text_to_sequence(text)
+            sequences.append(sequence)
+        return sequences
+
+    def sequence_to_text(self, sequence):
+        return ''.join(list(map(lambda i: self.itos[i], sequence)))
+
+    def sequences_to_texts(self, sequences):
+        texts = []
+        for sequence in sequences:
+            text = self.sequence_to_text(sequence)
+            texts.append(text)
+        return texts
+
+    def predict_caption(self, sequence):
+        caption = ''
+        for i in sequence:
+            if i == self.stoi['<eos>'] or i == self.stoi['<pad>']:
+                break
+            caption += self.itos[i]
+        return caption
+
+    def predict_captions(self, sequences):
+        captions = []
+        for sequence in sequences:
+            caption = self.predict_caption(sequence)
+            captions.append(caption)
+        return captions
+
+def get_score(y_true, y_pred):
+    scores = []
+    for true, pred in zip(y_true, y_pred):
+        score = Levenshtein.distance(true, pred)
+        scores.append(score)
+    avg_score = np.mean(scores)
+    return avg_score
+
+def init_logger(log_file):
+    from logging import getLogger, INFO, FileHandler,  Formatter,  StreamHandler
+    logger = getLogger(__name__)
+    logger.setLevel(INFO)
+    handler1 = StreamHandler()
+    handler1.setFormatter(Formatter("%(message)s"))
+    handler2 = FileHandler(filename=log_file)
+    handler2.setFormatter(Formatter("%(message)s"))
+    logger.addHandler(handler1)
+    logger.addHandler(handler2)
+    return logger
+
+def seed_torch(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def get_train_file_path(image_id):
+    return "../train/{}/{}/{}/{}.png".format(
+        image_id[0], image_id[1], image_id[2], image_id
+    )
+
+# ====================================================
+# Helper functions
+# ====================================================
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (remain %s)' % (asMinutes(s), asMinutes(rs))
