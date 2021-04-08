9768f14e3fed1f32722836387f5e5496b75949d5
Shun Zheng <shun.zhen@microsoft.com>
2021-04-08 11:39:15 +0000

    q

--- src/run.ep.sh
+++ src/run.ep.sh
@@ -8,10 +8,10 @@ unzip -q bms-molecular-translation.zip
 echo "install packages..."
 cd src
 pip install -r requirements.txt
-echo "preprocess..."
-python preprocess.py
-echo "save..."
-cp ../tokenizer.pth /mnt/epblob/zhgao/MT
-cp ../train.pkl /mnt/epblob/zhgao/MT
+# echo "preprocess..."
+# python preprocess.py
+# echo "save..."
+# cp ../tokenizer.pth /mnt/epblob/zhgao/MT
+# cp ../train.pkl /mnt/epblob/zhgao/MT
 
-python -m torch.distributed.launch --nproc_per_node 4 baseline_ep.py --model_name tf_efficientnet_b7 --batch_size 4 --size 448 --debug 1 -meta_info fordebug
+python -m torch.distributed.launch --nproc_per_node 1 baseline_ep.py --model_name tf_efficientnet_b7 --batch_size 4 --size 448 --debug 1 --meta_info fordebug
