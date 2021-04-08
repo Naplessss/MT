5ae7b8a3442b33f5ce28b87021c1dadbdf387ae0
root <root@pai-worker-28.cluster.local>
2021-04-08 11:23:38 +0000

    add ep

--- src/run.ep.sh
+++ src/run.ep.sh
@@ -1,13 +1,16 @@
+cd /root
 git clone http://Naplessss:GZF1233gzf@github.com/Naplessss/MT.git
 cd MT
-pip install -r requirements.txt
 echo "copy images from blob..."
 cp /mnt/epblob/zhgao/MT/bms-molecular-translation.zip .
 echo "unzip images ..."
 unzip -q bms-molecular-translation.zip
+echo "install packages..."
+cd src
+pip install -r requirements.txt
 echo "preprocess..."
 python preprocess.py
 echo "save..."
-cp tokenizer.pth /mnt/epblob/zhgao/MT
-cp train.pkl /mnt/epblob/zhgao/MT
+cp ../tokenizer.pth /mnt/epblob/zhgao/MT
+cp ../train.pkl /mnt/epblob/zhgao/MT
 
